6d67d904006df26c39e5052a076fc13e384fd01a
Majeed Thaika <majeed.thaika@hotmail.com>
2016-05-31 20:26:28 +0700

    Migrated Sam's code to current workspace

--- Squickly_Monsters/home.lua
+++ Squickly_Monsters/home.lua
@@ -3,8 +3,8 @@ local widget = require( "widget" )
 local composer = require( "composer" )
 local scene = composer.newScene()
 
-require("homepage.tama")
 require("homepage.background")
+require("homepage.monster")
 require("homepage.UI")
 
 -- -----------------------------------------------------------------------------------------------------------------
@@ -17,47 +17,76 @@ require("homepage.UI")
 
 -- -------------------------------------------------------------------------------
 
--- Writted functions go HERE
+-- Longer written functions go HERE
 
 
 
 -- -------------------------------------------------------------------------------
 
+-- Scene functions go HERE
+
 function scene:create( event )
 	local sceneGroup = self.view
 
-	-- Called when the scene's view does not exist.
-	-- 
-	-- INSERT code here to initialize the scene
-	-- e.g. add display objects to 'sceneGroup', add touch listeners, etc.
+    -- Setup layer
+    local back = display.newGroup()
+    local middle = display.newGroup()
+    local front = display.newGroup()
+	
+	-- Set background
+    background = setUpBackground("img/bg/main_background.png")
+	
+    -- Set Up Monster 
+    local tama = setUpMonster("img/sprites/egg_sprites_all.png")
 
-	-- display a background image
-	local background = display.newImageRect( "background.jpg", display.contentWidth, display.contentHeight )
-	background.anchorX = 0
-	background.anchorY = 0
-	background.x, background.y = 0, 0
-	
-	-- create/position logo/title image on upper-half of the screen
-	local titleLogo = display.newImageRect( "logo.png", 264, 42 )
-	titleLogo.x = display.contentWidth * 0.5
-	titleLogo.y = 100
-	
-	-- create a widget button (which will loads level1.lua on release)
-	playBtn = widget.newButton{
-		label="Play Now",
-		labelColor = { default={255}, over={128} },
-		default="button.png",
-		over="button-over.png",
-		width=154, height=40,
-		onRelease = onPlayBtnRelease	-- event listener function
-	}
-	playBtn.x = display.contentWidth*0.5
-	playBtn.y = display.contentHeight - 125
-	
-	-- all display objects must be inserted into group
-	sceneGroup:insert( background )
-	sceneGroup:insert( titleLogo )
-	sceneGroup:insert( playBtn )
+    -- Set up Needs Bar
+    hunger = setUpNeedBar("img/others/widget-progress-view.png")
+
+    hunger:setProgress( 0.5 )
+
+
+    setUpIcons()
+    local feedIcon = getFeedIcon()
+
+    -- Set reaction when touch
+    function tama:touch(event)
+        if event.phase == "ended" then
+            hideShowIcons(tama)
+        end
+    end
+
+    function feedIcon:touch(event)
+        if event.phase == "ended" then
+            feed()
+            hunger:setProgress(hunger:getProgress() + 0.1)
+            return true
+        end
+    end
+
+    -- Trigger needs
+    function needs()
+        hunger:setProgress(hunger:getProgress() - 0.1)
+    end
+
+	-- Add display objects into group
+    -- ============BACK===============
+    back:insert(background)
+    back:insert(feedIcon)
+
+    -- ===========MIDDLE==============
+    middle:insert(tama)
+
+    -- ===========FRONT===============
+    front:insert(hunger)
+
+
+    -- ===============================
+
+    -- Add hunger loop
+    timer.performWithDelay(20000, needs, -1)
+    -- Add even listener for touch event on tama
+    tama:addEventListener("touch", tama)
+    feedIcon:addEventListener("touch", feedIcon)
 end
 
 function scene:show( event )
