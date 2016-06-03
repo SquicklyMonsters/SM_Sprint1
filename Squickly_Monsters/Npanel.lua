00119ee47120390f2335eb9aecd83f7b9796f16e
BossyMU <Torpong.boss@gmail.com>
2016-06-03 14:32:17 +0700

    pair_coding
    menubar

---
+++ Squickly_Monsters/Npanel.lua
@@ -0,0 +1,49 @@
+
+local widget = require("widget")
+local composer = require("composer")
+--=====================================================================================================================================================
+-- This is sliding panel function. Have 2 command show, hide,
+function widget.newPanel( options )                                     -- Setting Panel : have default value and can be customize
+    local customOptions = options or {}
+    local opt = {}
+    opt.location = "right"
+    local default_width = display.contentWidth * 0.3
+    local default_height = display.contentHeight
+    opt.width = customOptions.width or default_width
+    opt.height = customOptions.height or default_height
+    opt.speed = customOptions.speed or 500
+    opt.inEasing = customOptions.inEasing or easing.linear
+    opt.outEasing = customOptions.outEasing or easing.linear
+
+    local container = display.newContainer( opt.width, opt.height )
+        container.anchorX = 0.0
+        container.x = display.actualContentWidth
+        container.anchorY = 0.5
+        container.y = display.contentCenterY
+    function container:show()                                             -- show function
+        local options = {
+            time = opt.speed,
+            transition = opt.inEasing
+        }
+        if ( opt.listener ) then
+            options.onComplete = opt.listener
+            self.completeState = "shown"
+        end
+        options.x = display.actualContentWidth - opt.width
+        transition.to( self, options )
+    end
+    function container:hide()                                           -- hide function
+        local options = {
+            time = opt.speed,
+            transition = opt.outEasing
+        }
+        if ( opt.listener ) then
+            options.onComplete = opt.listener
+            self.completeState = "hidden"
+        end
+        options.x = display.actualContentWidth
+        transition.to( self, options )
+    end
+    return container
+end
+-- ==========================================================================================================================
