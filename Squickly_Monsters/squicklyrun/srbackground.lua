cc13000a82b0d3856148594253c4e5da76fbdde5
Majeed Thaika <majeed.thaika@hotmail.com>
2016-06-10 13:24:59 +0700

    Added obstacles and pits

--- Squickly_Monsters/squicklyrun/srbackground.lua
+++ Squickly_Monsters/squicklyrun/srbackground.lua
@@ -24,14 +24,18 @@ local speed;
 local hero;
 local collisionRect;
 
+--these 2 variables will be the checks that control our event system.
+local inEvent;
+local eventRun;
+
 -- -----------------------------------------------------------------------------------------------------------------
 
 function getScreenLayer()
-    return screen
+	return screen
 end
 
 function getPlayerLayer()
-    return player
+	return player
 end
 
 -- -----------------------------------------------------------------------------------------------------------------
@@ -69,7 +73,11 @@ function setupGround()
 	groundMin = 420
 	groundMax = 340
 	groundLevel = groundMin
-	speed = 5;
+	speed = 5
+
+	--Event identifiers
+	inEvent = 0
+	eventRun = 0
 
 	--this for loop will generate all of your ground pieces, we are going to
 	--make 8 in all.
@@ -140,23 +148,23 @@ end
 --Update functions
 
 function update( event )
-    updateBackgrounds()
-    updateSpeed()
-    updateHero()
-    updateBlocks()
-    checkCollisions()
+	updateBackgrounds()
+	updateSpeed()
+	updateHero()
+	updateBlocks()
+	checkCollisions()
 end
 
 function updateBackgrounds()
 	--far background movement
 	backgroundfar.x = backgroundfar.x - (speed/55)
-	 
+	
 	--near background movement
 	backgroundnear1.x = backgroundnear1.x - (speed/5)
 	if(backgroundnear1.x < -239) then
 		backgroundnear1.x = 760
 	end
-	 
+	
 	backgroundnear2.x = backgroundnear2.x - (speed/5)
 	if(backgroundnear2.x < -239) then
 		backgroundnear2.x = 760
@@ -164,33 +172,33 @@ function updateBackgrounds()
 end
 
 function updateSpeed()
-	speed = speed + .05
+	speed = speed + .0005
 end
 
 function updateHero()
-     --if our hero is jumping then switch to the jumping animation
-     --if not keep playing the running animation
-     if(onGround) then
-          if(wasOnGround == false) then
-               hero:setSequence("running")
-               hero:play()
-          end
-     else
-          hero:setSequence("jumping")
-          hero:play()
-     end
+	--if our hero is jumping then switch to the jumping animation
+	--if not keep playing the running animation
+	if(onGround) then
+		if(wasOnGround == false) then
+			hero:setSequence("running")
+			hero:play()
+		end
+	else
+		hero:setSequence("jumping")
+		hero:play()
+	end
  
-     if(hero.accel > 0) then
-          hero.accel = hero.accel - 1
-     end
+	if(hero.accel > 0) then
+		hero.accel = hero.accel - 1
+	end
  
-     --update the heros position accel is used for our jump and
-     --gravity keeps the hero coming down. You can play with those 2 variables
-     --to make lots of interesting combinations of gameplay like 'low gravity' situations
-     hero.y = hero.y - hero.accel
-     hero.y = hero.y - hero.gravity
-     --update the collisionRect to stay in front of the hero
-     collisionRect.y = hero.y
+	--update the heros position accel is used for our jump and
+	--gravity keeps the hero coming down. You can play with those 2 variables
+	--to make lots of interesting combinations of gameplay like 'low gravity' situations
+	hero.y = hero.y - hero.accel
+	hero.y = hero.y - hero.gravity
+	--update the collisionRect to stay in front of the hero
+	collisionRect.y = hero.y
 end
 
 function updateBlocks()
@@ -200,9 +208,13 @@ function updateBlocks()
 		else
 			newX = (blocks[8]).x + 79 - speed
 		end
-		 
 		if((blocks[a]).x < -40) then
-			(blocks[a]).x, (blocks[a]).y = newX, (blocks[a]).y
+			if(inEvent == 11) then
+				(blocks[a]).x, (blocks[a]).y = newX, 600
+			else
+				(blocks[a]).x, (blocks[a]).y = newX, groundLevel
+			end
+			checkEvent()
 		else
 			(blocks[a]):translate(speed * -1, 0)
 		end
@@ -210,27 +222,27 @@ function updateBlocks()
 end
  
 function checkCollisions()
-     wasOnGround = onGround
-     --checks to see if the collisionRect has collided with anything.
-     for a = 1, blocks.numChildren, 1 do
-          if(collisionRect.y - 10 > blocks[a].y - 170 and blocks[a].x - 40 < collisionRect.x and blocks[a].x + 40 > collisionRect.x) then
-               speed = 0
-          end
-     end
-     --this is where we check to see if the hero is on the ground or in the air, if he is in the air then he can't jump(sorry no double
-     --jumping for our little hero, however if you did want him to be able to double jump like Mario then you would just need
-     --to make a small adjustment here, by adding a second variable called something like hasJumped. Set it to false normally, and turn it to
-     --true once the double jump has been made. That way he is limited to 2 hops per jump.
-     --Again we cycle through the blocks group and compare the x and y values of each.
-     for a = 1, blocks.numChildren, 1 do
-          if(hero.y >= blocks[a].y - 170 and blocks[a].x < hero.x + 60 and blocks[a].x > hero.x - 60) then
-               hero.y = blocks[a].y - 171
-               onGround = true
-               break
-          else
-               onGround = false
-          end
-     end
+	wasOnGround = onGround
+	--checks to see if the collisionRect has collided with anything.
+	for a = 1, blocks.numChildren, 1 do
+		if(collisionRect.y - 10 > blocks[a].y - 170 and blocks[a].x - 40 < collisionRect.x and blocks[a].x + 40 > collisionRect.x) then
+			speed = 0
+		end
+	end
+	--this is where we check to see if the hero is on the ground or in the air, if he is in the air then he can't jump(sorry no double
+	--jumping for our little hero, however if you did want him to be able to double jump like Mario then you would just need
+	--to make a small adjustment here, by adding a second variable called something like hasJumped. Set it to false normally, and turn it to
+	--true once the double jump has been made. That way he is limited to 2 hops per jump.
+	--Again we cycle through the blocks group and compare the x and y values of each.
+	for a = 1, blocks.numChildren, 1 do
+		if(hero.y >= blocks[a].y - 170 and blocks[a].x < hero.x + 60 and blocks[a].x > hero.x - 60) then
+			hero.y = blocks[a].y - 171
+			onGround = true
+			break
+		else
+			onGround = false
+		end
+	end
 end
 
 -- -----------------------------------------------------------------------------------------------------------------
@@ -238,12 +250,77 @@ end
 
 --this is the function that handles the jump events. If the screen is touched on the left side
 --then make the monster jump
+function checkEvent()
+	--first check to see if we are already in an event, we only want 1 event going on at a time
+	if(eventRun > 0) then
+		 --if we are in an event decrease eventRun. eventRun is a variable that tells us how
+		  --much longer the event is going to take place. Everytime we check we need to decrement
+		  --it. Then if at this point eventRun is 0 then the event has ended so we set inEvent back
+		  --to 0.
+		eventRun = eventRun - 1
+		if(eventRun == 0) then
+			inEvent = 0
+		end
+	end
+	 --if we are in an event then do nothing
+	if(inEvent > 0 and eventRun > 0) then
+		  --Do nothing
+	else
+		  --if we are not in an event check to see if we are going to start a new event. To do this
+		  --we generate a random number between 1 and 100. We then check to see if our 'check' is
+		  --going to start an event. We are using 100 here in the example because it is easy to determine
+		  --the likelihood that an event will fire(We could just as easilt chosen 10 or 1000).
+		  --For example, if we decide that an event is going to
+		  --start everytime check is over 80 then we know that everytime a block is reset there is a 20%
+		  --chance that an event will start. So one in every five blocks should start a new event. This
+		  --is where you will have to fit the needs of your game.
+		check = math.random(100)
+ 
+		  --this first event is going to cause the elevation of the ground to change. For this game we
+		  --only want the elevation to change 1 block at a time so we don't get long runs of changing
+		  --elevation that is impossible to pass so we set eventRun to 1.
+		if(check > 80 and check < 99) then
+			   --since we are in an event we need to decide what we want to do. By making inEvent another
+			   --random number we can now randomly choose which direction we want the elevation to change.
+			inEvent = math.random(10)
+			eventRun = 1
+		end
+	end
+	 --if we are in an event call runEvent to figure out if anything special needs to be done
+	if(inEvent > 0) then
+		runEvent()
+	end
+	
+	if(check > 98) then
+		inEvent = 11
+		eventRun = 2
+	end
+end
+
+--this function is pretty simple it just checks to see what event should be happening, then
+--updates the appropriate items. Notice that we check to make sure the ground is within a
+--certain range, we don't want the ground to spawn above or below whats visible on the screen.
+function runEvent()
+	if(inEvent < 6) then
+		groundLevel = groundLevel + 40
+	end
+	if(inEvent > 5 and inEvent < 11) then
+		groundLevel = groundLevel - 40
+	end
+	if(groundLevel < groundMax) then
+		groundLevel = groundMax
+	end
+	if(groundLevel > groundMin) then
+		groundLevel = groundMin
+	end
+end
+
 function touched( event )
-     if(event.phase == "began") then
-          if(event.x < 241) then
-               if(onGround) then
-                    hero.accel = hero.accel + 20
-               end
-          end
-     end
+	if(event.phase == "began") then
+		if(event.x < 241) then
+			if(onGround) then
+				hero.accel = hero.accel + 20
+			end
+		end
+	end
 end
