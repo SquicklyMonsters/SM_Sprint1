57b509cdd5abbb4833c088bfb069fe1858656589
pankaj.oa.09 <pankaj.oa.09@gmail.com>
2016-06-14 00:48:21 +0700

    Got the currency on shop screen and game rewards kinda work

---
+++ Squickly_Monsters/currency.lua
@@ -0,0 +1,62 @@
+local goldMoney = 300 --default goldMoney
+local goldAmount
+
+
+-- These really do what they say they do, pretty self explanatory
+
+--FOR GOLD
+
+function increaseGold(goldAmount)
+	goldMoney = goldMoney + goldAmount
+	print("goldMoney" .. goldMoney)
+end
+
+function decreaseGold(goldAmount)
+	if (goldMoney - goldAmount) >= 0 then
+		goldMoney = goldMoney - goldAmount
+		print("goldMoney" .. goldMoney)	
+	end
+
+end
+
+function checkSufficientGold(goldAmount)
+	if (goldMoney - goldAmount) >= 0 then
+		return true	
+	else
+		return false
+	end
+end
+
+function returnCurrentGold()
+	return goldMoney
+end
+
+-- -----------------------------------------------------------------------
+--FOR PLATINUM
+
+local platinumMoney = 0 --default platinumMoney
+local platinumAmount
+
+function increasePlatinum(platinumAmount)
+	platinumMoney = platinumMoney + platinumAmount
+	print("platinumMoney" .. goldMoney)
+end
+
+function decreasePlatinum(platinumAmount)
+	if (platinumMoney - platinumAmount) >= 0 then
+		platinumMoney = platinumMoney - platinumAmount
+		print("platinumMoney" .. goldMoney)
+	end
+end
+
+function checkSufficientPlatinum(platinumAmount)
+	if (platinumMoney - platinumAmount) >= 0 then
+		return true	
+	else
+		return false
+	end
+end
+
+function returnCurrentPlatinum()
+	return platinumMoney
+end
