c3abdc2839d01cbf8ac587772faeb8d746a9221c
BossyMU <Torpong.boss@gmail.com>
2016-06-15 19:37:59 +0700

    commit

--- Squickly_Monsters/inventory.lua
+++ Squickly_Monsters/inventory.lua
@@ -22,7 +22,7 @@ function itemClickedEvent(event)
 		if quantity > 0 then
 			-- Update display number
 			itemTexts[idx].text = quantity
-		else 
+		else
 			updateInventory()
 		end
 		food:eat()
@@ -46,7 +46,7 @@ function updateInventory()
 end
 -- -------------------------------------------------------------------------------
 
-function widget.newPanel(options)                                    
+function widget.newPanel(options)
     local background = display.newImage(options.imageDir)
     local container = display.newContainer(options.width, options.height)
     container:insert(background, true)
@@ -76,7 +76,7 @@ function setUpInventory()
 
  	for i = 1, #itemList do --loops to create each item on inventory
  		local x = startX + (spacingX * ((i-1) - math.floor((i-1)/rows)*rows))
- 		local y = startY + (spacingY * (math.floor((i-1) / rows))) 
+ 		local y = startY + (spacingY * (math.floor((i-1) / rows)))
  		local food = foodList[itemList[i]]
  		inventory.items[i] = widget.newButton {
  			top = y, -- division of row
@@ -90,10 +90,10 @@ function setUpInventory()
  		inventory.items[i].item = food
  		inventory.items[i].idx = i
  		local textOptions = {
-			text = itemQuantities[i], 
-			x = x + 70, 
-			y = y + 65, 
-			width = 50, 
+			text = itemQuantities[i],
+			x = x + 70,
+			y = y + 65,
+			width = 50,
 			height = 50
  		}
 
@@ -118,7 +118,7 @@ function setUpInventory()
 
  	inventory:insert(inventory.close)
  	inventory:scale(
- 				(display.contentWidth/inventory.width)*0.4, 
+ 				(display.contentWidth/inventory.width)*0.4,
  				(display.contentHeight/inventory.height)*0.5
  				)
 
@@ -143,7 +143,7 @@ function setUpInventory()
     local goldText = display.newText(GoldOptions)
     goldText:setFillColor( 255/255, 223/255, 0 )
     inventory:insert(goldText)
-        
+
     local platinumText = display.newText(PlatinumOptions)
     platinumText:setFillColor( 229/255, 228/255, 226/255 )
     inventory:insert(platinumText)
@@ -163,7 +163,7 @@ end
 -- Called when the scene's view does not exist:
 function scene:create( event )
 	local sceneGroup = self.view
-	inventory = setUpInventory()
+	inventory = setUpInventory()	
 	sceneGroup:insert(inventory)
 	print(composer.getSceneName("current"))
 
@@ -172,7 +172,7 @@ end
 function scene:show( event )
 	local sceneGroup = self.view
 	local phase = event.phase
-    
+
 
 	if phase == "will" then
 		-- composer.hideOverlay()
