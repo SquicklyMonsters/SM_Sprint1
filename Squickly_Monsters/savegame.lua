32e57be7213ae0ca901e0edee7b7c5e354f692c4
samx1118 <samx_48783_@hotmail.com>
2016-06-30 21:35:30 +0700

    Data Sage Final Testing

--- Squickly_Monsters/savegame.lua
+++ Squickly_Monsters/savegame.lua
@@ -1,75 +1,75 @@
-require("homepage.UI")
-local json = require("json")
--- -----------------------------------------------------------------------------------------------------------------
--- Some forward declarations
--- local maxNeedsLevels;
--- local needsLevels;
-local saveRate;
+-- require("homepage.UI")
+-- local json = require("json")
+-- -- -----------------------------------------------------------------------------------------------------------------
+-- -- Some forward declarations
+-- -- local maxNeedsLevels;
+-- -- local needsLevels;
+-- local saveRate;
 
--- Set location for saved data
-local needsDataFile = system.pathForFile( "needsData.txt", system.DocumentsDirectory )
-local inventoryDataFile = system.pathForFile( "inventoryData.txt", system.DocumentsDirectory )
-local rewardDateDataFile = system.pathForFile( "rewardsData.txt", system.DocumentsDirectory )
+-- -- Set location for saved data
+-- local needsDataFile = system.pathForFile( "needsData.txt", system.DocumentsDirectory )
+-- local inventoryDataFile = system.pathForFile( "inventoryData.txt", system.DocumentsDirectory )
+-- local rewardDateDataFile = system.pathForFile( "rewardsData.txt", system.DocumentsDirectory )
 
--- -------------------------------------------------------------------------------
--- Set Auto Save rate
--- function setAutoSaveRate(rate) -- 1000 = 1sec
--- 	if saveRate~= nil then
--- 		timer.cancel(saveRate)
--- 	end
---     saveRate = timer.performWithDelay(rate, saveAllData, -1)
+-- -- -------------------------------------------------------------------------------
+-- -- Set Auto Save rate
+-- -- function setAutoSaveRate(rate) -- 1000 = 1sec
+-- -- 	if saveRate~= nil then
+-- -- 		timer.cancel(saveRate)
+-- -- 	end
+-- --     saveRate = timer.performWithDelay(rate, saveAllData, -1)
+-- -- end
+
+-- -- -------------------------------------------------------------------------------
+-- function writeFile(file, contents)
+--     file = io.open(file, "w")
+--     file:write(contents)
+--     io.close(file)    
+-- end
+-- -- -------------------------------------------------------------------------------
+-- -- Save functions
+-- function saveAllData()
+--     saveNeedsData()
+--     -- saveInventoryData()
 -- end
 
--- -------------------------------------------------------------------------------
-function writeFile(file, contents)
-    file = io.open(file, "w")
-    file:write(contents)
-    io.close(file)    
-end
--- -------------------------------------------------------------------------------
--- Save functions
-function saveAllData()
-    saveNeedsData()
-    -- saveInventoryData()
-end
-
-function saveNeedsData()
-	-- print("saved file")
-    local maxNeedsLevels = getMaxNeedsLevels()
-    local needsLevels = getCurrentNeedsLevels()
-    local monsterLevel = getMonsterLevel()
+-- function saveNeedsData()
+-- 	-- print("saved file")
+--     local maxNeedsLevels = getMaxNeedsLevels()
+--     local needsLevels = getCurrentNeedsLevels()
+--     local monsterLevel = getMonsterLevel()
     
-    local outTable = {maxNeedsLevels, needsLevels, monsterLevel}
-    local contents = json.encode(outTable)
+--     local outTable = {maxNeedsLevels, needsLevels, monsterLevel}
+--     local contents = json.encode(outTable)
         
-    writeFile(needsDataFile, contents)
-    print("save")
-end
+--     writeFile(needsDataFile, contents)
+--     print("save")
+-- end
 
-function saveInventoryData()
-    local itemList = getItemList()
-    local foodRecentList = getFoodRecentList()
-    local playRecentList = getPlayRecentList()
-    local itemQuantities = getItemQuantities()
-    local gold = getCurrentGold()
-    local platinum = getCurrentPlatinum()
+-- function saveInventoryData()
+--     local itemList = getItemList()
+--     local foodRecentList = getFoodRecentList()
+--     local playRecentList = getPlayRecentList()
+--     local itemQuantities = getItemQuantities()
+--     local gold = getCurrentGold()
+--     local platinum = getCurrentPlatinum()
 
-    local outTable = {itemList, foodRecentList, playRecentList, itemQuantities, gold, platinum}
-    local contents = json.encode(outTable)
+--     local outTable = {itemList, foodRecentList, playRecentList, itemQuantities, gold, platinum}
+--     local contents = json.encode(outTable)
 
 
-    writeFile(inventoryDataFile, contents)
-    print("save inv")
-end
+--     writeFile(inventoryDataFile, contents)
+--     print("save inv")
+-- end
 
-function saveRewardTimerData()
-    -- only save when reward is obtained by player
-    local currentDate = os.date( '*t' )
+-- function saveRewardTimerData()
+--     -- only save when reward is obtained by player
+--     local currentDate = os.date( '*t' )
 
-    local outTable = {currentDate}
-    local contents = json.encode(outTable)
+--     local outTable = {currentDate}
+--     local contents = json.encode(outTable)
 
-    writeFile(rewardDateDataFile, contents)
-    print( os.date( "%c" ) )
-    print("save date")
-end
+--     writeFile(rewardDateDataFile, contents)
+--     print( os.date( "%c" ) )
+--     print("save date")
+-- end
