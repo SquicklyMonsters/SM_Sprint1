382dff45e1e22dc500fe095a7c46e186eccfeb19
Majeed Thaika <majeed.thaika@hotmail.com>
2016-07-16 02:49:53 +0700

    Fixed Bossy's merge problems

--- Squickly_Monsters/shop/interactions.lua
+++ Squickly_Monsters/shop/interactions.lua
@@ -1,43 +1,46 @@
--- -----------------------------------------------------------------------------
+local composer = require("composer")
+require("data")
+-- -------------------------------------------------------------------------------
 -- Local variables go HERE
 
-local backgroundShop;
+local background;
+
+local notifications;
 
 -- -------------------------------------------------------------------------------
 
-function setUpBackground()
-    -- Set Background
-    -- local background = display.newImage("background.png", display.contentCenterX, display.contentCenterY)
-    -- local backgroundOption = {
-    --     width = 1024,
-    --     height = 768,
-    --     numFrames = 1,
-
-    --     -- sheetContentWidth = 1024,
-    --     -- sheetContentHeight = 768,
-
-    -- }
-    -- local backgroundSheet = graphics.newImageSheet("img/bg/shop.png", backgroundOption)
-    -- local backgroundSequence = {
-    --     start = 1,
-    --     count = 8,
-    --     time = 1400,
-    --     loopcount = 0,
-    --     loopdirection = "forward"
-    -- }
-    -- backgroundShop = display.newSprite(backgroundSheet, backgroundSequence)
-    -- backgroundShop.x = display.contentCenterX
-    -- backgroundShop.y = display.contentCenterY
-    -- backgroundShop:scale(
-    --                 display.contentWidth/backgroundOption.width,
-    --                 display.contentHeight/backgroundOption.height
-    --                 )
-    backgroundShop = display.newImage("img/bg/shop.png", display.contentCenterX, display.contentCenterY)
-    backgroundShop:scale(display.contentWidth/backgroundShop.width, display.contentHeight/backgroundShop.height )
+function cacheVariables()
+    background = getBackground()
 end
 
-function getBackground()
-    return backgroundShop
+function buyNotice(i)
+    notifications[i].alpha = 1
+    transition.fadeOut( notifications[i], { time=500 } )
 end
+
+function setUpNotifications()
+    local boughtNotice = display.newImageRect("img/icons/UIIcons/buy.png", 150, 150)
+    boughtNotice.x = display.contentCenterX
+    boughtNotice.y = display.contentCenterY
+    boughtNotice.alpha = 0
+
+    local cantBuyNotice = display.newImageRect("img/icons/UIIcons/cannotbuy.png", 150, 150)
+    cantBuyNotice.x = display.contentCenterX
+    cantBuyNotice.y = display.contentCenterY
+    cantBuyNotice.alpha = 0
+
+    notifications = {boughtNotice, cantBuyNotice}
+    return notifications
+end
+
+function refreshDisplayCurrency(goldText, platinumText)
+    goldText.text = "Gold: " .. getGold()
+    platinumText.text = "Platinum: " .. getPlatinum()
+end
+-- -------------------------------------------------------------------------------
 
 -- -------------------------------------------------------------------------------
+-- Hide / Show Icons with Lock
+
+-- -------------------------------------------------------------------------------
+-- Add All Event Listeners Here
